06b0a67ef4292948c6bf732b4764197c662fa5b4
Casimir <jul.lutz@gmx.net>
2013-07-20 17:33:52 +0200

    add flowers-mod

---
+++ mods/flowers/init.lua
@@ -0,0 +1,121 @@
+-- Minetest 0.4 mod: default
+-- See README.txt for licensing and other information.
+
+-- Map Generation
+dofile(minetest.get_modpath("flowers").."/mapgen.lua")
+
+-- Aliases for original flowers mod
+minetest.register_alias("flowers:flower_dandelion_white", "flowers:dandelion_white")
+minetest.register_alias("flowers:flower_dandelion_yellow", "flowers:dandelion_yellow")
+minetest.register_alias("flowers:flower_geranium", "flowers:geranium")
+minetest.register_alias("flowers:flower_rose", "flowers:rose")
+minetest.register_alias("flowers:flower_tulip", "flowers:tulip")
+minetest.register_alias("flowers:flower_viola", "flowers:viola")
+
+minetest.register_node("flowers:dandelion_white", {
+	description = "White Dandelion",
+	drawtype = "plantlike",
+	tiles = { "flowers_dandelion_white.png" },
+	inventory_image = "flowers_dandelion_white.png",
+	wield_image = "flowers_dandelion_white.png",
+	sunlight_propagates = true,
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	groups = {snappy=3,dig_immediate=3,flammable=2,flower=1,flora=1,attached_node=1,color_white=1},
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
+	},
+})
+
+minetest.register_node("flowers:dandelion_yellow", {
+	description = "Yellow Dandelion",
+	drawtype = "plantlike",
+	tiles = { "flowers_dandelion_yellow.png" },
+	inventory_image = "flowers_dandelion_yellow.png",
+	wield_image = "flowers_dandelion_yellow.png",
+	sunlight_propagates = true,
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	groups = {snappy=3,dig_immediate=3,flammable=2,flower=1,flora=1,attached_node=1,color_yellow=1},
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
+	},
+})
+
+minetest.register_node("flowers:geranium", {
+	description = "Blue Geranium",
+	drawtype = "plantlike",
+	tiles = { "flowers_geranium.png" },
+	inventory_image = "flowers_geranium.png",
+	wield_image = "flowers_geranium.png",
+	sunlight_propagates = true,
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	groups = {snappy=3,dig_immediate=3,flammable=2,flower=1,flora=1,attached_node=1,color_blue=1},
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
+	},
+})
+
+minetest.register_node("flowers:rose", {
+	description = "Rose",
+	drawtype = "plantlike",
+	tiles = { "flowers_rose.png" },
+	inventory_image = "flowers_rose.png",
+	wield_image = "flowers_rose.png",
+	sunlight_propagates = true,
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	groups = {snappy=3,dig_immediate=3,flammable=2,flower=1,flora=1,attached_node=1,color_red=1},
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
+	},
+})
+
+minetest.register_node("flowers:tulip", {
+	description = "Tulip",
+	drawtype = "plantlike",
+	tiles = { "flowers_tulip.png" },
+	inventory_image = "flowers_tulip.png",
+	wield_image = "flowers_tulip.png",
+	sunlight_propagates = true,
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	groups = {snappy=3,dig_immediate=3,flammable=2,flower=1,flora=1,attached_node=1,color_orange=1},
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
+	},
+})
+
+minetest.register_node("flowers:viola", {
+	description = "Viola",
+	drawtype = "plantlike",
+	tiles = { "flowers_viola.png" },
+	inventory_image = "flowers_viola.png",
+	wield_image = "flowers_viola.png",
+	sunlight_propagates = true,
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	groups = {snappy=3,dig_immediate=3,flammable=2,flower=1,flora=1,attached_node=1,color_violet=1},
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
+	},
+})
