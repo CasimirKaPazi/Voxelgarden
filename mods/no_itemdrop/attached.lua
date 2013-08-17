cb46aad5b58741b00b90ca0bf79097c3b69aeb9d
Casimir <jul.lutz@gmx.net>
2013-08-18 00:46:14 +0200

    cleanup attached

--- mods/no_itemdrop/attached.lua
+++ mods/no_itemdrop/attached.lua
@@ -1,12 +1,10 @@
 function drop_attached_node(p)
-	local nn = minetest.get_node(p).name
+	local node = minetest.get_node(p)
 	minetest.remove_node(p)
-	for _,item in ipairs(minetest.get_node_drops(nn, "")) do
-		local pos = {
-			x = math.floor(p.x + 0.5),
-			y = math.floor(p.y + 0.5),
-			z = math.floor(p.z + 0.5),
-		}
-		spawn_falling_node(pos, {name=item})
-	end
+	local pos = {
+		x = math.floor(p.x + 0.5),
+		y = math.floor(p.y + 0.5),
+		z = math.floor(p.z + 0.5),
+	}
+	spawn_falling_node(pos, node)
 end
