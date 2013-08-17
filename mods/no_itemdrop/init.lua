73334bc221e0e628b3f3f089b2ab61bd39764c0c
Casimir <jul.lutz@gmx.net>
2013-08-17 23:15:00 +0200

    disable no_itemdrop as long as it is broken

--- mods/no_itemdrop/init.lua
+++ mods/no_itemdrop/init.lua
@@ -1,3 +1,3 @@
-dofile(minetest.get_modpath(minetest.get_current_modname()).."/overwrite.lua")
-dofile(minetest.get_modpath(minetest.get_current_modname()).."/attached.lua")
+--dofile(minetest.get_modpath(minetest.get_current_modname()).."/overwrite.lua")
+--dofile(minetest.get_modpath(minetest.get_current_modname()).."/attached.lua")
 dofile(minetest.get_modpath(minetest.get_current_modname()).."/fullinventory.lua")
