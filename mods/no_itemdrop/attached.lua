5755cba38effd4b8bfac0d5c0ee7b84ba7828885
Casimir <jul.lutz@gmx.net>
2013-08-17 23:39:24 +0200

    fix attached

--- mods/no_itemdrop/attached.lua
+++ mods/no_itemdrop/attached.lua
@@ -1,12 +1,12 @@
 function drop_attached_node(p)
-	local nn = minetest.env:get_node(p).name
-	minetest.env:remove_node(p)
+	local nn = minetest.get_node(p).name
+	minetest.remove_node(p)
 	for _,item in ipairs(minetest.get_node_drops(nn, "")) do
 		local pos = {
 			x = math.floor(p.x + 0.5),
 			y = math.floor(p.y + 0.5),
 			z = math.floor(p.z + 0.5),
 		}
-		spawn_falling_node(pos, item)
+		spawn_falling_node(pos, {name=item})
 	end
 end
