ec6dc4c340c1ed33e03e7ed4be2672a49fe42ae2
Casimir <jul.lutz@gmx.net>
2013-10-18 20:23:53 +0200

    recipe for sting

--- mods/farming/cotton.lua
+++ mods/farming/cotton.lua
@@ -112,6 +112,13 @@ minetest.register_craftitem("farming:string", {
 })
 
 minetest.register_craft({
+	output = "farming:string 2",
+	recipe = {
+			{"farming:cotton", "farming:cotton"},
+			}
+})
+
+minetest.register_craft({
 	output = "wool:white",
 	recipe = {
 			{"farming:cotton", "farming:cotton", "farming:cotton"},
