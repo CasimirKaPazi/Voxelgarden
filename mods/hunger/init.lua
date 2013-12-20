af04542ae3f2e4363ecd512ce696f32b3770d163
Casimir <jul.lutz@gmx.net>
2013-12-20 11:52:05 +0100

    hunger depends on health

--- mods/hunger/init.lua
+++ mods/hunger/init.lua
@@ -1,30 +1,40 @@
-if minetest.setting_getbool("enable_damage") == true then
-
-players_hunger = {}
+hunger = {}
+players_hunger	= {}
+players_timer	= {}
 
 -- no_drown privilege
 minetest.register_privilege("no_hunger", {
-	description = "Player wont feel hunger",
+	description = "Player will feel no hunger",
 	give_to_singleplayer = false
 })
+
+if minetest.setting_getbool("enable_damage") == true then
 
 local timer = 0
 minetest.register_globalstep(function(dtime)
 	timer = timer + dtime;
-	if timer >= 180 then --720 for one day
-		timer = 0
-		for _,player in ipairs(minetest.get_connected_players()) do
-			local name = player:get_player_name()
-			if players_hunger[name] == nil then
-				players_hunger[name] = 0
-			end
+	if timer < 1 then return end
+	timer = 0
+	for _,player in ipairs(minetest.get_connected_players()) do
+		local name = player:get_player_name()
+		if players_hunger[name] == nil then
+			players_hunger[name] = 0
+		end
+		if players_timer[name] == nil then
+			players_timer[name] = 0
+		end
+		players_timer[name] = players_timer[name] + 1
+		local health = player:get_hp()
+		local timer = 180 * health / 20
+		if players_timer[name] >= timer then  --720 equals one day
 			players_hunger[name] = 1
---			print("[hunger] hunger for "..name.."")
-        end
-    end
+			players_timer[name] = 0
+		end
+--		print("[hunger] hunger for "..name.."")
+	end
 end)
 
-do_hunger = function(player)
+hunger.check = function(player)
 	if not player then return end
 	local name = player:get_player_name()
 	if minetest.get_player_privs(name)["no_hunger"] then
@@ -48,11 +58,11 @@ do_hunger = function(player)
 end
 
 minetest.register_on_dignode(function(pos, oldnode, digger)
-	minetest.after(10, do_hunger, digger)
+	minetest.after(10, hunger.check, digger)
 end)
 
 minetest.register_on_placenode(function(pos, node, placer)
-	minetest.after(10, do_hunger, placer)
+	minetest.after(10, hunger.check, placer)
 end)
 
 end
