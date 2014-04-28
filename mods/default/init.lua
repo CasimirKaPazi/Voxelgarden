061a90e38faf0409b9a1bf0b61198fca2a008713
Casimir <jul.lutz@gmx.net>
2014-04-29 00:36:49 +0200

    remove support for mapgen v7

--- mods/default/init.lua
+++ mods/default/init.lua
@@ -1,8 +1,6 @@
 -- Minetest 0.4 mod: default
 -- See README.txt for licensing and other information.
 
--- The API documentation in here was moved into doc/lua_api.txt
-
 WATER_ALPHA = 160
 WATER_VISC = 1
 LAVA_VISC = 7
@@ -30,25 +28,3 @@ dofile(minetest.get_modpath("default").."/mapgen.lua")
 dofile(minetest.get_modpath("default").."/leafdecay.lua")
 dofile(minetest.get_modpath("default").."/player.lua")
 dofile(minetest.get_modpath("default").."/trees.lua")
-if minetest.setting_get("mg_name") == "v7" then
-	dofile(minetest.get_modpath("default").."/biomes.lua")
-end
-
-
--- Support old code
-function default.spawn_falling_node(p, nodename)
-	spawn_falling_node(p, nodename)
-end
-
--- Horrible crap to support old code
--- Don't use this and never do what this does, it's completely wrong!
--- (More specifically, the client and the C++ code doesn't get the group)
-function default.register_falling_node(nodename, texture)
-	minetest.log("error", debug.traceback())
-	minetest.log('error', "WARNING: default.register_falling_node is deprecated")
-	if minetest.registered_nodes[nodename] then
-		minetest.registered_nodes[nodename].groups.falling_node = 1
-	end
-end
-
--- END
