d8f0a5899da44ec715410359b3e24b9b1140bf34
Casimir <jul.lutz@gmx.net>
2014-10-01 14:01:14 +0200

    Use firelike drawtype

--- mods/fire/init.lua
+++ mods/fire/init.lua
@@ -4,7 +4,7 @@ minetest.register_alias("fire", "fire:basic_flame")
 
 minetest.register_node("fire:basic_flame", {
 	description = "Fire",
-	drawtype = "plantlike",
+	drawtype = "firelike",
 	tiles = {{
 		name="fire_basic_flame_animated.png",
 		animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=1},
