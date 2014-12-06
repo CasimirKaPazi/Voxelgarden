416e60aee7e121c9671dbe725676172666d2c1c9
Casimir <jul.lutz@gmx.net>
2014-12-06 18:25:40 +0100

    Fix various fire sound bugs, add setting disable_fire

--- mods/fire/init.lua
+++ mods/fire/init.lua
@@ -1,5 +1,7 @@
 -- minetest/fire/init.lua
 
+fire = {}
+
 minetest.register_alias("fire", "fire:basic_flame")
 
 minetest.register_node("fire:basic_flame", {
@@ -18,17 +20,14 @@ minetest.register_node("fire:basic_flame", {
 	buildable_to = true,
 	damage_per_second = 4,
 	sunlight_propagates = true,
-	
-	after_place_node = function(pos, placer)
-		fire.on_flame_add_at(pos)
+	on_construct = function(pos)
+		minetest.after(0, fire.on_flame_add_at, pos)
 	end,
-	
-	after_dig_node = function(pos, oldnode, oldmetadata, digger)
-		fire.on_flame_remove_at(pos)
-	end,
+	on_destruct = function(pos)
+		minetest.after(0, fire.on_flame_remove_at, pos)
+ 	end,
 })
 
-fire = {}
 fire.D = 6
 -- key: position hash of low corner of area
 -- value: {handle=sound handle, name=sound name}
@@ -54,8 +53,6 @@ function fire.update_sounds_around(pos)
 	local p0, p1 = fire.get_area_p0p1(pos)
 	local cp = {x=(p0.x+p1.x)/2, y=(p0.y+p1.y)/2, z=(p0.z+p1.z)/2}
 	local flames_p = minetest.find_nodes_in_area(p0, p1, {"fire:basic_flame"})
-	--print("number of flames at "..minetest.pos_to_string(p0).."/"
-	--		..minetest.pos_to_string(p1)..": "..#flames_p)
 	local should_have_sound = (#flames_p > 0)
 	local wanted_sound = nil
 	if #flames_p >= 9 then
@@ -87,18 +84,14 @@ function fire.update_sounds_around(pos)
 end
 
 function fire.on_flame_add_at(pos)
-	--print("flame added at "..minetest.pos_to_string(pos))
 	fire.update_sounds_around(pos)
 	flame_count = flame_count + 1
---	print("[fire] flame_count = "..flame_count.."")
 end
 
 function fire.on_flame_remove_at(pos)
-	--print("flame removed at "..minetest.pos_to_string(pos))
 	fire.update_sounds_around(pos)
 	flame_count = flame_count - 1
 	if flame_count < 0 then flame_count = 0 end
---	print("[fire] flame_count = "..flame_count.."")
 end
 
 function fire.find_pos_for_flame_around(pos)
@@ -106,6 +99,7 @@ function fire.find_pos_for_flame_around(pos)
 end
 
 function fire.flame_should_extinguish(pos)
+	if minetest.setting_getbool("disable_fire") then return true end
 	--return minetest.find_node_near(pos, 1, {"group:puts_out_fire"})
 	local p0 = {x=pos.x-2, y=pos.y, z=pos.z-2}
 	local p1 = {x=pos.x+2, y=pos.y, z=pos.z+2}
@@ -130,7 +124,6 @@ minetest.register_abm({
 		local p = fire.find_pos_for_flame_around(p0)
 		if p then
 			minetest.set_node(p, {name="fire:basic_flame"})
-			fire.on_flame_add_at(p)
 		end
 	end,
 })
@@ -144,7 +137,6 @@ minetest.register_abm({
 		-- If there is water or stuff like that around flame, remove flame
 		if fire.flame_should_extinguish(p0) then
 			minetest.remove_node(p0)
-			fire.on_flame_remove_at(p0)
 			return
 		end
 		-- Make the following things rarer
@@ -154,7 +146,6 @@ minetest.register_abm({
 		-- If there are no flammable nodes around flame, remove flame
 		if not minetest.find_node_near(p0, 1, {"group:flammable"}) then
 			minetest.remove_node(p0)
-			fire.on_flame_remove_at(p0)
 			return
 		end
 		if math.random(1,8) == 1 then
@@ -166,13 +157,11 @@ minetest.register_abm({
 					return
 				end
 				minetest.set_node(p, {name="fire:basic_flame"})
-				fire.on_flame_add_at(p)
 				nodeupdate(p)
 			end
 		else
 			-- Remove flame
 			minetest.remove_node(p0)
-			fire.on_flame_remove_at(p0)
 		end
 	end,
 })
