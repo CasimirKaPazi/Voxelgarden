7d903032026e85ba3a132b1c4cb480c8215ab5dc
Casimir <jul.lutz@gmx.net>
2014-12-06 21:01:16 +0100

    Register flowers in tables

--- mods/flowers/init.lua
+++ mods/flowers/init.lua
@@ -12,112 +12,36 @@ minetest.register_alias("flowers:flower_rose", "flowers:rose")
 minetest.register_alias("flowers:flower_tulip", "flowers:tulip")
 minetest.register_alias("flowers:flower_viola", "flowers:viola")
 
+local flowers = {
+--	flower name,		desc			color
+	{"dandelion_white",	"White Dandelion",	{dig_immediate=3, flammable=2, flower=1, flora=1, attached_node=1, dissolve=1, color_white=1}},
+	{"dandelion_yellow",	"Yellow Dandelion",	{dig_immediate=3, flammable=2, flower=1, flora=1, attached_node=1, dissolve=1, color_yellow=1}},
+	{"geranium",		"Blue Geranium",	{dig_immediate=3, flammable=2, flower=1, flora=1, attached_node=1, dissolve=1, color_blue=1}},
+	{"rose",		"Rose",			{dig_immediate=3, flammable=2, flower=1, flora=1, attached_node=1, dissolve=1, color_red=1}},
+	{"tulip",		"Tulip",		{dig_immediate=3, flammable=2, flower=1, flora=1, attached_node=1, dissolve=1, color_orange=1}},
+	{"viola",		"Viola",		{dig_immediate=3, flammable=2, flower=1, flora=1, attached_node=1, dissolve=1, color_violet=1}},
+}
+
 -- Flowers
-
-minetest.register_node("flowers:dandelion_white", {
-	description = "White Dandelion",
-	drawtype = "plantlike",
-	tiles = { "flowers_dandelion_white.png" },
-	inventory_image = "flowers_dandelion_white.png",
-	wield_image = "flowers_dandelion_white.png",
-	sunlight_propagates = true,
-	paramtype = "light",
-	walkable = false,
-	buildable_to = true,
-	groups = {snappy=3, dig_immediate=3, flammable=2, flower=1, flora=1, attached_node=1, dissolve=1, color_white=1},
-	sounds = default.node_sound_leaves_defaults(),
-	selection_box = {
-		type = "fixed",
-		fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
-	},
-})
-
-minetest.register_node("flowers:dandelion_yellow", {
-	description = "Yellow Dandelion",
-	drawtype = "plantlike",
-	tiles = { "flowers_dandelion_yellow.png" },
-	inventory_image = "flowers_dandelion_yellow.png",
-	wield_image = "flowers_dandelion_yellow.png",
-	sunlight_propagates = true,
-	paramtype = "light",
-	walkable = false,
-	buildable_to = true,
-	groups = {snappy=3, dig_immediate=3, flammable=2, flower=1, flora=1, attached_node=1, dissolve=1, color_yellow=1},
-	sounds = default.node_sound_leaves_defaults(),
-	selection_box = {
-		type = "fixed",
-		fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
-	},
-})
-
-minetest.register_node("flowers:geranium", {
-	description = "Blue Geranium",
-	drawtype = "plantlike",
-	tiles = { "flowers_geranium.png" },
-	inventory_image = "flowers_geranium.png",
-	wield_image = "flowers_geranium.png",
-	sunlight_propagates = true,
-	paramtype = "light",
-	walkable = false,
-	buildable_to = true,
-	groups = {snappy=3, dig_immediate=3, flammable=2, flower=1, flora=1, attached_node=1, dissolve=1, color_blue=1},
-	sounds = default.node_sound_leaves_defaults(),
-	selection_box = {
-		type = "fixed",
-		fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
-	},
-})
-
-minetest.register_node("flowers:rose", {
-	description = "Rose",
-	drawtype = "plantlike",
-	tiles = { "flowers_rose.png" },
-	inventory_image = "flowers_rose.png",
-	wield_image = "flowers_rose.png",
-	sunlight_propagates = true,
-	paramtype = "light",
-	walkable = false,
-	buildable_to = true,
-	groups = {snappy=3, dig_immediate=3, flammable=2, flower=1, flora=1, attached_node=1, dissolve=1, color_red=1},
-	sounds = default.node_sound_leaves_defaults(),
-	selection_box = {
-		type = "fixed",
-		fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
-	},
-})
-
-minetest.register_node("flowers:tulip", {
-	description = "Tulip",
-	drawtype = "plantlike",
-	tiles = { "flowers_tulip.png" },
-	inventory_image = "flowers_tulip.png",
-	wield_image = "flowers_tulip.png",
-	sunlight_propagates = true,
-	paramtype = "light",
-	walkable = false,
-	buildable_to = true,
-	groups = {snappy=3, dig_immediate=3, flammable=2, flower=1, flora=1, attached_node=1, dissolve=1, color_orange=1},
-	sounds = default.node_sound_leaves_defaults(),
-	selection_box = {
-		type = "fixed",
-		fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
-	},
-})
-
-minetest.register_node("flowers:viola", {
-	description = "Viola",
-	drawtype = "plantlike",
-	tiles = { "flowers_viola.png" },
-	inventory_image = "flowers_viola.png",
-	wield_image = "flowers_viola.png",
-	sunlight_propagates = true,
-	paramtype = "light",
-	walkable = false,
-	buildable_to = true,
-	groups = {snappy=3, dig_immediate=3, flammable=2, flower=1, flora=1, attached_node=1, dissolve=1, color_violet=1},
-	sounds = default.node_sound_leaves_defaults(),
-	selection_box = {
-		type = "fixed",
-		fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
-	},
-})
+for _, row in ipairs(flowers) do
+	local name = row[1]
+	local desc = row[2]
+	local groups = row[3]
+	minetest.register_node("flowers:"..name, {
+		description = desc,
+		drawtype = "plantlike",
+		tiles = {"flowers_"..name..".png"},
+		inventory_image = "flowers_"..name..".png",
+		wield_image = "flowers_"..name..".png",
+		sunlight_propagates = true,
+		paramtype = "light",
+		walkable = false,
+		buildable_to = true,
+		groups = groups,
+		sounds = default.node_sound_leaves_defaults(),
+		selection_box = {
+			type = "fixed",
+			fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
+		},
+	})
+end
