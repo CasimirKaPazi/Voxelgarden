870793909e4ef5b794aeb7f34df63ef4022a21fa
CasimirKaPazi <jul.lutz@gmx.net>
2017-05-10 20:56:45 +0200

    flowers: Use decoration

--- mods/flowers/mapgen.lua
+++ mods/flowers/mapgen.lua
@@ -1,72 +1,66 @@
-minetest.register_on_generated(function(minp, maxp, seed)
-	if maxp.y <= 128 and minp.y >= -32 then
-		-- Generate flowers
-		local perlin1 = minetest.get_perlin(436, 3, 0.6, 100)
-		-- Assume X and Z lengths are equal
-		local divlen = 16
-		local divs = (maxp.x-minp.x)/divlen+1;
-		for divx=0, divs-1 do
-		for divz=0, divs-1 do
-			local x0 = minp.x + math.floor((divx+0)*divlen)
-			local z0 = minp.z + math.floor((divz+0)*divlen)
-			local x1 = minp.x + math.floor((divx+1)*divlen)
-			local z1 = minp.z + math.floor((divz+1)*divlen)
-			-- Determine flowers amount from perlin noise
-			local grass_amount = math.floor(perlin1:get2d({x=x0, y=z0}) ^ 3 * 9)
-			-- Find random positions for flowers based on this random
-			local pr = PseudoRandom(seed+456)
-			for i=0, grass_amount do
-				local x = pr:next(x0, x1)
-				local z = pr:next(z0, z1)
-				-- Find ground level
-				local ground_y = nil
-				for y=maxp.y, minp.y, -1 do
-					local nn = minetest.get_node({x=x, y=y, z=z}).name
-					if nn ~= "air" and nn~= "ignore" then
-						local is_leaves = minetest.registered_nodes[nn].groups.leaves
-						if is_leaves == nil or is_leaves == 0 then
-							ground_y = y
-							break
-						end
-					end
-				end
-				if not ground_y then return end
-				local p = {x=x, y=ground_y+1, z=z}
-				local nn = minetest.get_node(p).name
-				-- Check if the node can be replaced
-				if minetest.registered_nodes[nn] and
-					minetest.registered_nodes[nn].buildable_to then
-					nn = minetest.get_node({x=x, y=ground_y, z=z}).name
-					if nn == "default:dirt_with_grass" then
-						local flower_choice = pr:next(1, 6)
-						local flower
-						if flower_choice == 1 then
-							flower = "flowers:tulip"
-						elseif flower_choice == 2 then
-							flower = "flowers:rose"
-						elseif flower_choice == 3 then
-							flower = "flowers:viola"
-						elseif flower_choice == 4 then
-							flower = "flowers:flower_geranium"
-						elseif flower_choice == 5 then
-							flower = "flowers:dandelion_white"
-						elseif flower_choice == 6 then
-							flower = "flowers:dandelion_yellow"
-						end
-						minetest.set_node(p, {name=flower})
-					elseif nn == "default:dirt" or nn == "default:stone" then
-						local flower_choice = pr:next(1, 2)
-						local flower
-						if flower_choice == 1 then
-							flower = "flowers:mushroom_brown"
-						else
-							flower = "flowers:mushroom_red"
-						end
-						minetest.set_node(p, {name=flower})
-					end
-				end
-			end
-		end
-		end
-	end
-end)
+local function register_mgv6_flower(name)
+	minetest.register_decoration({
+		deco_type = "simple",
+		place_on = {"default:dirt_with_grass"},
+		sidelen = 16,
+		noise_params = {
+			offset = 0,
+			scale = 0.006,
+			spread = {x = 100, y = 100, z = 100},
+			seed = 436,
+			octaves = 3,
+			persist = 0.5
+		},
+		y_min = 1,
+		y_max = 300,
+		decoration = "flowers:"..name,
+	})
+end
+
+local function register_mgv6_mushroom(name)
+	minetest.register_decoration({
+		deco_type = "simple",
+		place_on = {"default:dirt_with_grass"},
+		sidelen = 16,
+		noise_params = {
+			offset = 0,
+			scale = 0.04,
+			spread = {x = 100, y = 100, z = 100},
+			seed = 7133,
+			octaves = 3,
+			persist = 0.6
+		},
+		y_min = 1,
+		y_max = 300,
+		decoration = "flowers:"..name,
+		spawn_by = "group:tree",
+		num_spawn_by = 1,
+	})
+end
+
+function flowers.register_mgv6_decorations()
+	register_mgv6_flower("rose")
+	register_mgv6_flower("tulip")
+	register_mgv6_flower("dandelion_yellow")
+	register_mgv6_flower("geranium")
+	register_mgv6_flower("viola")
+	register_mgv6_flower("dandelion_white")
+
+	register_mgv6_mushroom("mushroom_brown")
+	register_mgv6_mushroom("mushroom_red")
+	-- TODO: Add waterlily
+--	register_mgv6_waterlily()
+end
+
+
+--
+-- Detect mapgen to select functions
+--
+
+-- TODO: Other mapgens
+-- local mg_name = minetest.get_mapgen_setting("mg_name")
+-- if mg_name == "v6" then
+	flowers.register_mgv6_decorations()
+-- else
+-- 	flowers.register_decorations()
+-- end
