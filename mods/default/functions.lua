28c327f98cc79fa1ea80335e40442a64a8e3667e
CasimirKaPazi <jul.lutz@gmx.net>
2017-05-25 15:19:46 +0200

    Don't remove water renewing yet

--- mods/default/functions.lua
+++ mods/default/functions.lua
@@ -190,6 +190,72 @@ minetest.register_abm({
 })
 
 --
+-- Water renewable
+--
+
+-- If the water edge is a straight line flowing has 3 neighbors,
+-- everything curved inwards would be more, anything outwards would be less.
+
+local function check_for_source(candidates, source)
+	if not source then return end
+	local count = 0
+	for _, pos in ipairs(candidates) do
+		if minetest.get_node(pos).name == source then
+			count = count +1
+		end
+	end
+	return count
+end
+
+local function renew_liquid(pos, source)
+		-- Direct neighbors
+		local px = {x = pos.x + 1, y = pos.y, z = pos.z}
+		local mx = {x = pos.x - 1, y = pos.y, z = pos.z}
+		local pz = {x = pos.x, y = pos.y, z = pos.z + 1}
+		local mz = {x = pos.x, y = pos.y, z = pos.z - 1}
+		local n_direct ={px, mx, pz, mz}
+		local count_direct = check_for_source(n_direct, source)
+		if count_direct <= 1 then return false end
+		-- Diagonal neighbors
+		local pxpz = {x = pos.x + 1, y = pos.y, z = pos.z + 1}
+		local pxmz = {x = pos.x + 1, y = pos.y, z = pos.z - 1}
+		local mxpz = {x = pos.x - 1, y = pos.y, z = pos.z + 1}
+		local mxmz = {x = pos.x - 1, y = pos.y, z = pos.z - 1}
+		local n_diagonal = {pxpz, pxmz, mxpz, mxmz}
+		local count_diagonal = check_for_source(n_diagonal, source)
+		if count_direct + count_diagonal >= 4 then
+			return true
+		end
+		return false
+end
+
+minetest.register_abm({
+	nodenames = {"default:water_flowing"},
+	neighbors = {"default:water_source"},
+	interval = 1,
+	chance = 5,
+	catch_up = false,
+	action = function(pos, node)
+		if renew_liquid(pos, "default:water_source") then
+			minetest.set_node(pos, {name = "default:water_source"})
+		end
+	end
+})
+
+minetest.register_abm({
+	nodenames = {"default:lava_flowing"},
+	neighbors = {"default:lava_source"},
+	interval = 1,
+	chance = 5,
+	catch_up = false,
+	action = function(pos, node)
+		if renew_liquid(pos, "default:lava_source") then
+			minetest.set_node(pos, {name = "default:lava_source"})
+		end
+	end
+})
+
+--
 -- Papyrus and cactus growing
 --
 
