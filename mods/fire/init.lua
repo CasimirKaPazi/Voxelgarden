f7c72b2bf946fc165dd7bc56c6a962a86b5be353
CasimirKaPazi <jul.lutz@gmx.net>
2017-05-26 02:36:35 +0200

    fire: Use node timer to remove flame

--- mods/fire/init.lua
+++ mods/fire/init.lua
@@ -20,10 +20,20 @@ minetest.register_node("fire:basic_flame", {
 	buildable_to = true,
 	sunlight_propagates = true,
 	damage_per_second = 4,
+	on_timer = function(pos)
+		-- If there are no flammable nodes around flame, remove flame
+		local p = minetest.find_node_near(pos, 1, {"group:flammable"})
+		if not p then
+			minetest.remove_node(pos)
+			return false
+		end
+		return true
+	end,
+	on_construct = function(pos)
+		minetest.get_node_timer(pos):start(math.random(5, 10))
+	end,
 })
 
--- Only ignite if there are at least two fire nods around
--- This enables infinite fire pits
 function fire.node_should_burn(pos)
 	local p0 = {x=pos.x-1, y=pos.y-1, z=pos.z-1}
 	local p1 = {x=pos.x+1, y=pos.y+1, z=pos.z+1}
@@ -38,33 +48,22 @@ minetest.register_abm({
 	nodenames = {"group:flammable"},
 	neighbors = {"group:igniter"},
 	interval = 1,
-	chance = 5,
+	chance = 8,
 	catch_up = false,
-	action = function(p0, node, _, _)
-		if fire.node_should_burn(p0) then
-			minetest.set_node(p0, {name="fire:basic_flame"})
+	action = function(pos, node, _, _)
+		if minetest.find_node_near(pos, 1, {"air"}) or
+				fire.node_should_burn(pos) then
+			minetest.set_node(pos, {name="fire:basic_flame"})
 		end
 	end,
 })
 
--- Remove flammable nodes and flame
-minetest.register_abm({
+minetest.register_lbm({
+	name = "fire:convert_fire_to_node_timer",
 	nodenames = {"fire:basic_flame"},
-	interval = 1,
-	chance = 8,
-	catch_up = false,
-	action = function(p0, node, _, _)
-		-- If there are no flammable nodes around flame, remove flame
-		local p = minetest.find_node_near(p0, 1, {"group:flammable"})
-		if not p then
-			minetest.remove_node(p0)
-			return
-		end
-		local p_air = minetest.find_node_near(p0, 1, {"air"})
-		if p_air then
-			minetest.set_node(p_air, {name="fire:basic_flame"})
-		end
-	end,
+	action = function(pos)
+		minetest.get_node_timer(pos):start(math.random(5, 10))
+	end
 })
 
 end
