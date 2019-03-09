c58e76f29167cb2ec68163e7c12d4156ce22c750
CasimirKaPazi <jul.lutz@gmx.net>
2019-03-09 23:41:27 +0100

    Don't stack rats

--- mods/mobs_flat/init.lua
+++ mods/mobs_flat/init.lua
@@ -7,31 +7,3 @@ dofile(path .. "/omsk.lua")
 dofile(path .. "/dustdevil.lua")
 dofile(path .. "/rat.lua")
 dofile(path .. "/dungeonmaster.lua")
-
-minetest.register_craftitem("mobs_flat:rat_caught" ,{
-	description = "Rat",
-	inventory_image = "mobs_flat_rat_caught.png",
-	wield_image = "mobs_flat_rat_caught.png^[transformR90",
-	on_place = function(itemstack, placer, pointed_thing)
-		if pointed_thing.type ~= "node" then
-			return
-		end
-		pointed_thing.under.y = pointed_thing.under.y + 1
-		minetest.env:add_entity(pointed_thing.under, "mobs_flat:rat")
-		itemstack:take_item()
-		return itemstack
-	end,
-})
-
-minetest.register_craftitem("mobs_flat:rat_cooked", {
-	description = "Cooked Rat",
-	inventory_image = "mobs_flat_rat_cooked.png",
-	on_use = minetest.item_eat(4),
-	cooktime = 5,
-})
-
-minetest.register_craft({
-	type = "cooking",
-	output = "mobs_flat:rat_cooked",
-	recipe = "mobs_flat:rat_caught",
-})
--- mods/mobs_flat/rat.lua
+++ mods/mobs_flat/rat.lua
@@ -42,5 +42,35 @@ mobs:spawn({
 	active_object_count = 2
 })
 
-
 mobs:register_egg("mobs_flat:rat", "Rat", "default_gravel.png", 1)
+
+-- Catch and cook delicious rats
+
+minetest.register_craftitem("mobs_flat:rat_caught" ,{
+	description = "Rat",
+	inventory_image = "mobs_flat_rat_caught.png",
+	wield_image = "mobs_flat_rat_caught.png^[transformR90",
+	stack_max = 1,
+	on_place = function(itemstack, placer, pointed_thing)
+		if pointed_thing.type ~= "node" then
+			return
+		end
+		pointed_thing.under.y = pointed_thing.under.y + 1
+		minetest.env:add_entity(pointed_thing.under, "mobs_flat:rat")
+		itemstack:take_item()
+		return itemstack
+	end,
+})
+
+minetest.register_craftitem("mobs_flat:rat_cooked", {
+	description = "Cooked Rat",
+	inventory_image = "mobs_flat_rat_cooked.png",
+	on_use = minetest.item_eat(4),
+	cooktime = 5,
+})
+
+minetest.register_craft({
+	type = "cooking",
+	output = "mobs_flat:rat_cooked",
+	recipe = "mobs_flat:rat_caught",
+})
