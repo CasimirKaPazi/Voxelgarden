bc92fc48c5309bc527d83115080dd2500b6cd0a5
CasimirKaPazi <jul.lutz@gmx.net>
2020-09-21 18:37:14 +0200

    Add missing function for locked chests to work

--- mods/default/functions.lua
+++ mods/default/functions.lua
@@ -346,3 +346,45 @@ function default.rotate_horizontal(pos)
 	end
 	minetest.set_node(pos, node)
 end
+
+--
+-- NOTICE: This method is not an official part of the API yet.
+-- This method may change in future.
+--
+
+function default.can_interact_with_node(player, pos)
+	if player and player:is_player() then
+		if minetest.check_player_privs(player, "protection_bypass") then
+			return true
+		end
+	else
+		return false
+	end
+
+	local meta = minetest.get_meta(pos)
+	local owner = meta:get_string("owner")
+
+	if not owner or owner == "" or owner == player:get_player_name() then
+		return true
+	end
+
+	-- Is player wielding the right key?
+	local item = player:get_wielded_item()
+	if minetest.get_item_group(item:get_name(), "key") == 1 then
+		local key_meta = item:get_meta()
+
+		if key_meta:get_string("secret") == "" then
+			local key_oldmeta = item:get_metadata()
+			if key_oldmeta == "" or not minetest.parse_json(key_oldmeta) then
+				return false
+			end
+
+			key_meta:set_string("secret", minetest.parse_json(key_oldmeta).secret)
+			item:set_metadata("")
+		end
+
+		return meta:get_string("key_lock_secret") == key_meta:get_string("secret")
+	end
+
+	return false
+end
