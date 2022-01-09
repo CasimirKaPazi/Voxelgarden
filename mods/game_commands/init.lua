b15899bcac0def1dc4d313bb6d76e47f72429756
CasimirKaPazi <jul.lutz@gmx.net>
2022-01-09 01:16:39 +0100

    Update env_sounds, weather and add game_commands

---
+++ mods/game_commands/init.lua
@@ -0,0 +1,31 @@
+-- game_commands/init.lua
+
+-- Load support for MT game translation.
+local S = minetest.get_translator("game_commands")
+
+
+minetest.register_chatcommand("respawn", { -- Better not name that command "kill me"
+	description = S("Respawn"),
+	func = function(name)
+		local player = minetest.get_player_by_name(name)
+		if player then
+			if minetest.settings:get_bool("enable_damage") then
+				player:set_hp(0)
+				return true
+			else
+				for _, callback in pairs(minetest.registered_on_respawnplayers) do
+					if callback(player) then
+						return true
+					end
+				end
+
+				-- There doesn't seem to be a way to get a default spawn pos
+				-- from the lua API
+				return false, S("No static_spawnpoint defined")
+			end
+		else
+			-- Show error message if used when not logged in, eg: from IRC mod
+			return false, S("You need to be online to respawn!")
+		end
+	end
+})
