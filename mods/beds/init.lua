99e2a0a6c77e2da8c99dfe800135d33b6aea248c
CasimirKaPazi <jul.lutz@gmx.net>
2022-02-14 22:58:48 +0100

    Beds: update based on MTG, no formspec, don't kick players

--- mods/beds/init.lua
+++ mods/beds/init.lua
@@ -1,3 +1,5 @@
+-- beds/init.lua
+
 -- Load support for MT game translation.
 local S = minetest.get_translator("beds")
 
@@ -6,11 +8,6 @@ beds.player = {}
 beds.bed_position = {}
 beds.pos = {}
 beds.spawn = {}
-
-beds.formspec = "size[8,11;true]" ..
-	"no_prepend[]" ..
-	"bgcolor[#080808BB;true]" ..
-	"button_exit[2,10;4,0.75;leave;Leave Bed]"
 
 local modpath = minetest.get_modpath("beds")
 
