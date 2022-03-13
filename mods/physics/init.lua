383df6a0260f5240dbafef949de211535a642b9c
CasimirKaPazi <jul.lutz@gmx.net>
2022-03-13 23:23:58 +0100

    Remove full-inventory restriction

--- mods/physics/init.lua
+++ mods/physics/init.lua
@@ -1,5 +1,4 @@
 dofile(minetest.get_modpath(minetest.get_current_modname()).."/falling.lua")
-dofile(minetest.get_modpath(minetest.get_current_modname()).."/fullinventory.lua")
 
 if minetest.settings:get_bool("physics_liquid_falling") then
 	dofile(minetest.get_modpath(minetest.get_current_modname()).."/liquids_falling.lua")
